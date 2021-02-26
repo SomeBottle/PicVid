9aaa757f0b0564cf80e0a04c5f7df2b0dbad4a4d
SomeBottle <583797659@qq.com>
2021-02-26 21:44:32 +0800

    Initial commit

---
+++ uploadAPI.php
@@ -0,0 +1,9 @@
+<?php
+/*PV.PHP UPLOAD PIC, return value:<pic url> or <bool:false> in stand of error*/
+function PVUpload($path){
+	//pic upload function
+	$rawdata=
+	$imgurl=
+	return [(empty($imgurl) ? false : $imgurl),$rawdata];
+}
+?>
